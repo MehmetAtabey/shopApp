88d10977b4ab62d3fefb0818624300ed304c7fb4
mehmetatabey <mehmet_atabey@yahoo.com>
2020-07-10 00:29:40 +0300

    modified

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shopApp/providers/order_provider.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +21,20 @@ class OrderItem extends StatelessWidget {
         child: Column(
           children: <Widget>[
             ListTile(
-              title: Text(order.amount.toString()),
-              subtitle: Text(order.dateTime.toString()),
-              trailing:
-                  IconButton(icon: Icon(Icons.expand_more), onPressed: null),
-            )
+              title: Text(widget.order.amount.toString()),
+              subtitle: Text(widget.order.dateTime.toString()),
+              trailing: IconButton(
+                  icon: Icon(Icons.expand_more),
+                  onPressed: () =>setState((){_expanded = !_expanded;}) ),
+            ),
+            if (_expanded)
+              Container(
+                height: min(widget.order.products.length * 20.0 + 100, 180),
+                child: ListView(children: widget.order.products.map((e) => Row(children: <Widget>[
+                  Text(e.title),
+                  Text(e.quantity.toString()),
+                ],)).toList(),)
+              )
           ],
         ));
   }
